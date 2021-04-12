600ffa08f17bb6e3e7e632788e00d58c6639e61d
Pablo Soifer <pablo.soifer@wwecorp.com>
2021-04-12 11:40:32 -0300

    Custom commands Chapter 21

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('search', (value)=>{
+    cy.fixture('index').then((index)=>{
+        cy.get(index.searchBox).type(value);
+        cy.get(index.searchButton).click();
+    })
+})
